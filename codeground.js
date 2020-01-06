25534b487e8c3a9bb82694848522c3aa45f4edc9
Pratik Basak <basak.pratik@outlook.com>
2020-01-07 01:46:40 +0530

    char count code play

--- codeground.js
+++ codeground.js
@@ -3,8 +3,7 @@ function text(str) {
   const result = {};
   for (let i = 0; i < str.length; i++) {
     let char = str.charAt(i);
-    if (char !== " ")
-      result[char] = (isNaN(result[char]) ? 1 : result[char] + 1);
+    if (char !== " ") result[char] = isNaN(result[char]) ? 1 : result[char] + 1;
   }
   console.log(result);
 }
@@ -23,4 +22,29 @@ function textConvert(str) {
   }
   console.log(result);
 }
-textConvert("toUpperCase toLowerCase");
+// textConvert("toUpperCase toLowerCase");
+
+// #3: sample input - aaaabbbcccaaeeffz, expected output - a4b3c3a2e2f2z
+function charCount(str) {
+  let result,
+    count = 1,
+    i;
+
+  if (!str) {
+    return "";
+  }
+
+  result = str.charAt(0);
+  for (i = 1; i < str.length; i++) {
+    if (str.charAt(i) !== str.charAt(i - 1)) {
+      result += count + str.charAt(i);
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  return result + count;
+}
+console.log(charCount("aabbcccaeeeeetz"));
+
+// #4: if input is ["r", "b", "w", "b", "w", "r", "w", "r"], output will be in the following order ["r", "r", "r", "b", "b", "w", "w", "w"], where r stands for red, w fo white, b for blue
